3631d9d0d07713ef834ee3b3b5ab2dbcb685360c
Bilguun <bbg.1015@gmail.com>
2020-08-31 15:07:26 -0500

    added interface for axios.get and for action types created enum

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Movie {
+  imdbID: string;
+  title: string;
+  year: string;
+}
 
 export const fetchMovies = (movieName: string) => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(
+    const response = await axios.get<Movie[]>(
       `http://www.omdbapi.com/?s=${movieName}&apikey=${process.env.REACT_APP_API_KEY}`
     );
 
     dispatch({
-      type: 'FETCH_MOVIES',
+      type: ActionTypes.fetchMovies,
       payload: response.data,
     });
   };
